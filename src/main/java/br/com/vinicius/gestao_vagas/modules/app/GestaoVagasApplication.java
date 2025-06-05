bb7a3e71ce18db7030cb2d8b870029f4905fb554
ViniciusBolandini <125499098+ViniciusBolandini@users.noreply.github.com>
2025-06-05 00:38:15 -0300

    Ajuste de properties e adicao de EntityScan

--- src/main/java/br/com/vinicius/gestao_vagas/modules/app/GestaoVagasApplication.java
+++ src/main/java/br/com/vinicius/gestao_vagas/modules/app/GestaoVagasApplication.java
@@ -2,10 +2,12 @@ package br.com.vinicius.gestao_vagas.modules.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"br.com.vinicius.gestao_vagas"})
+@EntityScan(basePackages = {"br.com.vinicius.gestao_vagas.modules.candidate.entity"})
 public class GestaoVagasApplication {
     public static void main(String[] args){
         SpringApplication.run(GestaoVagasApplication.class, args);
--- src/main/java/br/com/vinicius/gestao_vagas/modules/candidate/entity/CandidateEntity.java
+++ src/main/java/br/com/vinicius/gestao_vagas/modules/candidate/entity/CandidateEntity.java
@@ -1,22 +1,33 @@
 package br.com.vinicius.gestao_vagas.modules.candidate.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
+@Entity(name = "candidate")
 public class CandidateEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
     @Pattern(regexp = "^(?!\\s*$).+", message = "O campo username não pode conter espaço")
-    private String usarname;
+    private String username;
     @Email(message = "O campo (email) deve conter um e-mail válido")
     private String email;
     @Length(min = 3)
     private String password;
     private String description;
     private String curriculum;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
